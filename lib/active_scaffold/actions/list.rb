3794386cc6463ca774c7e493039114e6a0bd21b9
scambra <sergio@entrecables.com>
2010-06-16 17:46:13 +0200

    Fix open nested scaffolds

--- lib/active_scaffold/actions/list.rb
+++ lib/active_scaffold/actions/list.rb
@@ -31,7 +31,7 @@ module ActiveScaffold::Actions
       render :action => 'list'
     end
     def list_respond_to_js
-      render :action => 'list.js'
+      render :action => params[:adapter] ? 'list.html' : 'list.js'
     end
     def list_respond_to_xml
       render :xml => response_object.to_xml(:only => active_scaffold_config.list.columns.names), :content_type => Mime::XML, :status => response_status
